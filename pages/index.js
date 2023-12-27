feab34c3c2ca0a34ebc21eb679a031c7adcf62bf
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2023-12-27 15:46:12 +0000

    adição arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
-    console.log("Servidor rodando!")
-    return <h1> Luana, eu te amo!</h1>
+  console.log("Servidor rodando!")
+  return <h1> Luana, eu te amo!</h1>
 }
 
 export default Home;
