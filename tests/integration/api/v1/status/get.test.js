dbf3f86fc9ff96c5a8632ab1c3b2c66d61f07f88
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2024-01-01 20:24:10 +0000

    adição do uso de variaveis de ambiente e do arquivo database.js

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("teste status response é 200", async () => {
+test("test/integration/api/status/get.test.js response é 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
