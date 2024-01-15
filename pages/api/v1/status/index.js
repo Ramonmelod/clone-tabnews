643840ae420f20679864a7f60ac0729b81f45377
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2024-01-15 17:40:06 +0000

    finish endpoint /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const dataBaseVersionResult = await database.query("show server_version;"); //controller
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+
+  const dataBaseMaxConnectionsResult = await database.query(
+    "show MAX_CONNECTIONS;",
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpennedConnectionsResult = await database.query({
+    // está também é a forma automatizada em que o node-postgres sanitiza a query
+    text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1",
+    values: [databaseName],
+  });
+
+  const databaseOpennedConnectionsValue =
+    databaseOpennedConnectionsResult.rows[0].count;
+  response.status(200).json({
+    updated_at: updatedAt, // view
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        open_connections: databaseOpennedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("test/integration/api/status/get.test.js response é 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const parsedupdatedAt = new Date(responseBody.updated_at).toISOString(); // recebe o valor vindo de updated_at dentro do new Date()
+
+  expect(responseBody.updated_at).toEqual(parsedupdatedAt);
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
