07b981ab7fe8bb755b355f564c64efb52ad8d4e6
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2024-03-22 19:03:33 +0000

    handles database ssl conection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true, // habilita a criptografia das informações trafegadas
   });
   console.log("Credenciais do postgres:", {
     host: process.env.POSTGRES_HOST,
