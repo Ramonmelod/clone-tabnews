90a2c0b370a5f892b4829bf6234adc7e3c133128
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2024-03-25 18:05:10 +0000

    initial migrations endpoin implementation

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,4 @@
+test("test/integration/api/migrations/get.test.js response é 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+});
