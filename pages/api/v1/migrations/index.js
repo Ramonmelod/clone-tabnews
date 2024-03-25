f4b43048f38837e3ad2625505937ed882f3c2de4
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2024-03-25 19:57:33 +0000

    initial implementation of the get method for the migration endPoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,14 @@
-import database from "infra/database";
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  response.status(200).json();
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"), // metodo join aplicado ao path para que caminho seja resolvido para diferentes S.O
+    direction: "up", // direção da migration
+    verbose: true,
+    migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
+  });
+  response.status(200).json(migrations);
 }
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,4 +1,8 @@
 test("test/integration/api/migrations/get.test.js response é 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(Array.isArray(responseBody)).toBe(true);
 });
