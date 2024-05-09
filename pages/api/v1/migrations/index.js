8d55bf37d0606a5fa5ecebc07ac0bd7f9b0fc366
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2024-05-09 19:36:39 +0000

    implementation of the post code and of the post.test.js file

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,13 +2,27 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"), // metodo join aplicado ao path para que caminho seja resolvido para diferentes S.O
-    direction: "up", // direção da migration
-    verbose: true,
-    migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
-  });
-  response.status(200).json(migrations);
+  if (request.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"), // metodo join aplicado ao path para que caminho seja resolvido para diferentes S.O
+      direction: "up", // direção da migration
+      verbose: true,
+      migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
+    });
+    return response.status(200).json(migrations);
+  }
+  if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"), // metodo join aplicado ao path para que caminho seja resolvido para diferentes S.O
+      direction: "up", // direção da migration
+      verbose: true,
+      migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
+    });
+    response.status(200).json(migrations);
+  }
+  response.status(405).end();
 }
