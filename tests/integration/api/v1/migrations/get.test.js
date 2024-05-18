d7824cab1a66200dbe457906ee376fd29c5d5c4e
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2024-05-18 21:55:13 +0000

    addition of the code tobeGreaterThan(0) in get.test.js

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,3 +1,5 @@
+import database from "infra/database";
+
 test("test/integration/api/migrations/get.test.js response é 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
@@ -5,4 +7,5 @@ test("test/integration/api/migrations/get.test.js response é 200", async () => 
   const responseBody = await response.json();
   console.log(responseBody);
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0); //see if the response comes empty
 });
