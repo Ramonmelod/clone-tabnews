a660e387c44fef6547e4c350a814a488f0f76105
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2024-11-21 22:10:31 +0000

    fix:`/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,31 +3,40 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient(); // recebe a conexão com o banco de dados vinda de getNewClient
-  if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      dbClient: dbClient,
-      dryRun: true,
-      dir: join("infra", "migrations"), // metodo join aplicado ao path para que caminho seja resolvido para diferentes S.O
-      direction: "up", // direção da migration (down)
-      verbose: true,
-      migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
-    });
-    await dbClient.end();
-    return response.status(200).json(migrations);
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    response
+      .status(405)
+      .json({ error: `Method ${request.method} is not allowed!` });
   }
-  if (request.method === "POST") {
-    const migrations = await migrationRunner({
-      dbClient: dbClient,
-      dryRun: false,
-      dir: join("infra", "migrations"), // metodo join aplicado ao path para que caminho seja resolvido para diferentes S.O
-      direction: "up", // direção da migration
-      verbose: true,
-      migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
-    });
-    await dbClient.end();
-    response.status(200).json(migrations);
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient(); // recebe a conexão com o banco de dados vinda de getNewClient
+    if (request.method === "GET") {
+      const migrations = await migrationRunner({
+        dbClient: dbClient,
+        dryRun: true,
+        dir: join("infra", "migrations"), // metodo join aplicado ao path para que caminho seja resolvido para diferentes S.O
+        direction: "up", // direção da migration (down)
+        verbose: true,
+        migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
+      });
+      return response.status(200).json(migrations);
+    }
+    if (request.method === "POST") {
+      const migrations = await migrationRunner({
+        dbClient: dbClient,
+        dryRun: false,
+        dir: join("infra", "migrations"), // metodo join aplicado ao path para que caminho seja resolvido para diferentes S.O
+        direction: "up", // direção da migration
+        verbose: true,
+        migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
+      });
+      response.status(200).json(migrations);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await dbClient.end(); //closing connection opened in dbClient to avoid opened connections for other methods
   }
-  response.status(405).end();
-  await dbClient.end(); //closing connection opened in dbClient to avoid opened connections for other methods
 }
