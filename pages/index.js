3ec36a8934d8bc54f2189bd3b130c078dd9045e8
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2025-01-24 16:56:59 +0000

    docs: erase the comment from the index.js in the pages directory

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   console.log("Servidor rodando!");
-  return <h1> Luana, eu te amo!</h1>; //here a component will be used
+  return <h1> Luana, eu te amo!</h1>;
 }
 
 export default Home;
