e88fc3400447da015797dde968cf2e9a808feb26
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2025-01-24 16:50:45 +0000

    docs: adds a comment in the index.js of the pages directory

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   console.log("Servidor rodando!");
-  return <h1> Luana, eu te amo!</h1>;
+  return <h1> Luana, eu te amo!</h1>; //here a component will be used
 }
 
 export default Home;
