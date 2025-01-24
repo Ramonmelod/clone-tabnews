3e060264afc030db7d5c8a69dda349f6eb7fa042
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2025-01-24 17:00:30 +0000

    docs: readd the comment

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   console.log("Servidor rodando!");
-  return <h1> Luana, eu te amo!</h1>;
+  return <h1> Luana, eu te amo!</h1>; //here will be a component
 }
 
 export default Home;
