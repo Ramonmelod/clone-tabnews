a88600d793e1fe90be62d125c23ca6c8688169ff
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2025-02-13 01:19:57 +0000

    refactor: `make /api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,53 +1,23 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database";
+import migrator from "models/migrator.js";
 
 const router = createRouter();
 export default router.handler(controller.errorHandlers);
 
 router.get(getHandler).post(postHandler);
 
-const defaultMigrationOptions = {
-  dryRun: false,
-  dir: resolve("infra", "migrations"), // metodo resolve aplicado ao path para que caminho seja resolvido para diferentes S.O
-  direction: "up", // direção da migration
-  verbose: true,
-  migrationsTable: "pgmigrations", // define a tabela de regitro das migrations
-};
-
 async function getHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient(); // recebe a conexão com o banco de dados vinda de getNewClient
-
-    const migrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient, //dbClient: dbClient,
-    });
-    return response.status(200).json(migrations);
-  } finally {
-    await dbClient?.end(); //closing connection opened in dbClient to avoid opened connections for other methods
-  }
+  const migrations = await migrator.listPendingMigrations();
+  return response.status(200).json(migrations);
 }
 
 async function postHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient(); // recebe a conexão com o banco de dados vinda de getNewClient
+  const migrations = await migrator.runPendingMigrations();
 
-    const migrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient, //dbClient: dbClient,
-    });
-
-    if (migrations.length > 0) {
-      return response.status(201).json(migrations);
-    }
-
-    return response.status(200).json(migrations);
-  } finally {
-    await dbClient?.end(); //closing connection opened in dbClient to avoid opened connections for other methods
+  if (migrations.length > 0) {
+    return response.status(201).json(migrations);
   }
+
+  return response.status(200).json(migrations);
 }
